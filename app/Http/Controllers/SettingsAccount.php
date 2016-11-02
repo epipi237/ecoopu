2eb963899bdc0e775ae1253beccc15644c1f41a4
katchoua <katchoua99@gmail.com>
2016-11-02 19:47:20 +0100

    Added more fields in the edit profile form

--- app/Http/Controllers/SettingsAccount.php
+++ app/Http/Controllers/SettingsAccount.php
@@ -3,9 +3,12 @@ namespace App\Http\Controllers;
 
 use Request;
 use Auth;
+use Validator;
 use App\User;
 use App\country;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Input;
 
 class SettingsAccount extends Controller
 {
@@ -19,10 +22,31 @@ class SettingsAccount extends Controller
 
 	public function update()
 	{
-		$user = Auth::user();
-		$user->name = Request::input('name');
-		$user->email = Request::input('email');
-		$user->save();
-		return redirect('/home');
+		$rules = array(
+			'name' => 'required|min:5',
+			'address' => 'required',
+			'phone' => 'required',
+			'username' => 'required|min:3',
+			'email' => 'required|email|max:255',
+			);
+		$messages = array(
+			'required' => 'The :attribute is required.',
+			'same'  => 'The :others must match.'
+			);
+		$validator = Validator::make(Input::all(), $rules);
+		if ($validator->fails()) {
+			$messages = $validator->messages();
+			return Redirect::back()->withErrors($validator)->withInput();
+
+		} else {
+			$user = Auth::user();
+			$user->name = Request::input('name');
+			$user->email = Request::input('email');
+			$user->phone = Request::input('phone');
+			$user->username = Request::input('username');
+			$user->address = Request::input('address');
+			$user->save() ;
+			return redirect('/home');
+		}
 	}
 }
--- resources/views/SettingsAccount/index.blade.php
+++ resources/views/SettingsAccount/index.blade.php
@@ -14,7 +14,7 @@
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
                             <div class="col-md-6">
-                            <input id="name" type="text" required="" class="form-control" name="name" value="{{ $user->name }}">
+                                <input id="name" type="text" required="" class="form-control" name="name" value="{{ $user->name }}">
 
                                 @if ($errors->has('name'))
                                 <span class="help-block">
@@ -23,6 +23,46 @@
                                 @endif
                             </div>
                         </div>
+
+                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <label for="username" class="col-md-4 control-label">Username</label>
+                            <div class="col-md-6">
+                                <input id="username" type="text" required="" class="form-control" name="username" value="{{ $user->username }}">
+
+                                @if ($errors->has('username'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('username') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">Telephone</label>
+                            <div class="col-md-6">
+                                <input id="phone" type="number" required="" class="form-control" name="phone" value="{{ $user->phone }}">
+
+                                @if ($errors->has('phone'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('phone') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">Address</label>
+                            <div class="col-md-6">
+                                <input id="address" type="text" required="" class="form-control" name="address" value="{{ $user->address }}">
+
+                                @if ($errors->has('address'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('address') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
