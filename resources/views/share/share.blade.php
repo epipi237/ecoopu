ddd25ac0950a7597f7a302489ad8901b277c6579
vaumi <vauvaumi@gmail.com>
2016-11-19 00:55:40 +0100

    facebook sharing

--- resources/views/share/share.blade.php
+++ resources/views/share/share.blade.php
@@ -4,7 +4,7 @@
   <a onclick='share("eCoopu","eCoopu.com","http://ecoopu.webshinobis.com/images/main-slider8.jpg","join me on {{$order->shop}}","Come join me so we get better deals")'>
   <i class="fa fa-facebook-official"></i>
 </a>
-<a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}"
+<a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}?text=come join me on {{$order->shop}}"
 target="_blank">
 <i class="fa fa-twitter-square"></i>
 </a>
