3ceac5acf813d8b40b8d31ba635430fd38175013
KATCHOUA God's Will <katchoua99@gmail.com>
2016-12-15 19:54:27 +0100

    Modified users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,16 +22,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-        $user = new User();
-        $user->name="Administator";
-        $user->username="Admin";
-        $user->address="St. Claire";
-        $user->email="admin@admin.com";
-        $user->phone="+237 676782064";
-        $user->role="admin";
-        $user->password=bcrypt("adminadmin");
-        $user->save();
     }
     
     /**
