e889f2833033a14aa7a58ed7aa6130068a30349d
Wadinga Leonard <wleo37@gmail.com>
2016-12-17 15:34:46 +0100

    some minor changes

--- resources/views/shop/list.blade.php
+++ resources/views/shop/list.blade.php
@@ -33,7 +33,7 @@
 							@endforeach
 						</tbody>
 					</table>
-					@if(!$price)
+					@if($price > 0)
 					<form class="form-horizontal" role="form" method="POST" action="{{route('order_price')}}">
 
 						{{ csrf_field() }}
