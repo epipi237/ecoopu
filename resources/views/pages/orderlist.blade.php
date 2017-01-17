089c3f8500fd657eb56675541ae2bf5ac3bdeb59
Wadinga Leonard <wleo37@gmail.com>
2017-01-17 06:02:03 +0100

    A slide test which might remain

--- resources/views/pages/orderlist.blade.php
+++ resources/views/pages/orderlist.blade.php
@@ -114,7 +114,7 @@
 							Pay now with PayPal or Credit Card
 							<br><br>
 
-							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<!-- <form action="https://www.paypal.com/cgi-bin/webscr" method="post" >
 								<input type="hidden" name="cmd" value="_xclick">
 
 								<input type="hidden" name="business" value="info@ecoopu.com">
@@ -125,12 +125,18 @@
 
 								<input type="hidden" name="quantity" value="1">
 
-								<input TYPE="hidden" name="charset" value="utf-8">
+								<input type="hidden" name="charset" value="utf-8">
 
 								<input type="hidden" name="hosted_button_id" value="ACW6S87267QSG">
 
 								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 								
+								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form> -->
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+								<input type="hidden" name="cmd" value="_s-xclick">
+								<input type="hidden" name="hosted_button_id" value="ACW6S87267QSG">
+								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 							</form>
 
