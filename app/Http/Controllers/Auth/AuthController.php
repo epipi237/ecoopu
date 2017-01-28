87de1977c711689efcc2cf9e0becdf78aa88ee3c
Wadinga Leonard <wleo37@gmail.com>
2017-01-28 16:42:31 +0100

    Did some cool changes

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -104,7 +104,7 @@ try {
        $validator = Validator::make(Input::all(), $rules);
        if ($validator->fails()) {
         $messages = $validator->messages();
-        return Redirect::back();
+        return \Redirect::back()->withErrors($validator)->withInput();
 
     } else {
 
